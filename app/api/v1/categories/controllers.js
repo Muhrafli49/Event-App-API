32b31788980b6af1e28aba68116dfb05b7a7befa
Rafli <mraflie49@gmail.com>
2024-08-24 12:23:21 +0700

    Update controllers

--- app/api/v1/categories/controllers.js
+++ app/api/v1/categories/controllers.js
@@ -1,10 +1,10 @@
-const Categories = require('./model');
+const { StatusCodes } = require('http-status-codes');
+const { getAllCategories, createCategories, getOneCategories, updateCategories, deleteCategories } = require('../../../services/mongoose/categories')
 
 const create = async (req, res, next) => {
     try {
-        const { name } = req.body;
-        const result = await Categories.create({ name });
-        res.status(201).json({
+        const result = await createCategories(req);
+        res.status(StatusCodes.CREATED).json({
             data: result,
         });
     } catch (err) {
@@ -14,8 +14,8 @@ const create = async (req, res, next) => {
 
 const index = async (req, res, next) => {
     try {
-        const result = await Categories.find().select('_id name');
-        res.status(200).json({
+        const result = await getAllCategories();
+        res.status(StatusCodes.OK).json({
             data: result,
         });
     } catch (err) {
@@ -26,28 +26,19 @@ const index = async (req, res, next) => {
 
 const find = async (req, res, next) => {
     try {
-        const { id } = req.params;
-        const result = await Categories.findOne({ _id: id});
-        res.status(200).json({
+        const result = await getOneCategories(req);
+        res.status(StatusCodes.OK).json({
             data: result,
         });
-    } catch {
+    } catch (err) {
         next(err)
     }
 }
 
 const update = async (req, res, next) => {
     try {
-        const { id } = req.params; 
-        const { name } = req.body;
-
-        const result = await Categories.findByIdAndUpdate(
-            { _id: id }, 
-            { name },
-            { new: true, runValidators: true }
-        );
-
-        res.status(200).json({
+        const result = await updateCategories(req);
+        res.status(StatusCodes.OK).json({
             data: result,
         });
     } catch (err) { 
@@ -57,10 +48,8 @@ const update = async (req, res, next) => {
 
 const destroy = async (req, res, next) => {
     try {
-        const { id } = req.params;
-        const result = await Categories.findByIdAndDelete(id);
-
-        res.status(200).json({
+        const result = await deleteCategories(req);
+        res.status(StatusCodes.OK).json({
             data: result,
         });
     } catch (err) {
