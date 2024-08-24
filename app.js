0f5294d3d0b79bce82063bfd52147c13e7102bc3
Rafli <mraflie49@gmail.com>
2024-08-24 12:27:00 +0700

    Add new dependencies and update app.js with necessary imports

--- app.js
+++ app.js
@@ -5,9 +5,17 @@ const logger = require('morgan');
 
 
 const app = express();
+
+// import router categories
 const categoriesRouter = require('./app/api/v1/categories/router');
 
+// inisiasi route v1
 const v1 = '/api/v1/cms';
+
+// import middleware
+const notFoundMiddleware = require('./app/middleware/not-found');
+const handlerErrorMiddleware = require('./app/middleware/handler-error');
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -15,13 +23,18 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+// test server
 app.get('/', (req, res) => {
     res.status(200).json({
         message: 'Welcome to the API'
     })
 })
 
+// Menggunakan API category
 app.use(v1, categoriesRouter);
+
+// Menggunakan handler error
+app.use(notFoundMiddleware);
+app.use(handlerErrorMiddleware);
 
 module.exports = app;
