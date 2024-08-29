35081bff8683cbf497125047d0cd260da93dc972
Rafli <mraflie49@gmail.com>
2024-08-29 10:17:17 +0700

    Update error handling module to include UnauthorizedError and UnauthenticatedError

--- app/errors/index.js
+++ app/errors/index.js
@@ -1,9 +1,14 @@
 const ResponseError = require('./response-error');
 const BadRequestError = require('./bad-request');
 const NotFoundError = require('./not-found');
+const UnauthorizedError = require('./unauthorized');
+const UnauthenticatedError = require('./unauthenticated');
+
 
 module.exports = {
     ResponseError,
     BadRequestError,
-    NotFoundError
+    NotFoundError,
+    UnauthorizedError,
+    UnauthenticatedError
 }
