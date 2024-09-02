1370677d4cb25d204372e49ce248d6d78550fec2
Rafli <mraflie49@gmail.com>
2024-09-02 09:35:44 +0700

    Add Gmail and password configuration from environment variables

--- app/config.js
+++ app/config.js
@@ -4,6 +4,8 @@ dotenv.config();
 module.exports = {
     urlDb: process.env.URL_MONGODB_DEV,
     jwtExpiration: '24h',
-    jwtSecret: process.env.JWT_SECRET
-}
+    jwtSecret: process.env.JWT_SECRET,
+    gmail: process.env.GMAIL,
+    password: process.env.PASSWORD,
+};
 
