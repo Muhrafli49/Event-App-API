fb7c3e81667423221ce39d278cbb75e9eeab2769
Rafli <mraflie49@gmail.com>
2024-09-02 09:37:55 +0700

    Add participant router import and use this router

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const logger = require('morgan');
 const app = express();
 
 // inisiasi route v1
-const v1 = '/api/v1/cms';
+const v1 = '/api/v1';
 
 // import router categories
 const categoriesRouter = require('./app/api/v1/categories/router');
@@ -30,6 +30,10 @@ const authCMSRouter = require('./app/api/v1/auth/router');
 // import router orders
 const ordersRouter = require('./app/api/v1/orders/router');
 
+// import router participants
+const participantsRouter = require('./app/api/v1/participants/router');
+
+
 // import middleware
 const notFoundMiddleware = require('./app/middleware/not-found');
 const handlerErrorMiddleware = require('./app/middleware/handler-error');
@@ -49,25 +53,28 @@ app.get('/', (req, res) => {
 })
 
 // Menggunakan API category
-app.use(v1, categoriesRouter);
+app.use(`${v1}/cms`, categoriesRouter);
 
 // Menggunakan API image
-app.use(v1, imagesRouter);
+app.use(`${v1}/cms`, imagesRouter);
 
 // Mengunakan API talent
-app.use(v1, talentsRouter);
+app.use(`${v1}/cms`, talentsRouter);
 
 // Mengunakan API event
-app.use(v1, eventsRouter);
+app.use(`${v1}/cms`, eventsRouter);
 
 // Mengunakan API organizer
-app.use(v1, organizersRouter);
+app.use(`${v1}/cms`, organizersRouter);
 
 // Mengunakan API organizer
-app.use(v1, authCMSRouter);
+app.use(`${v1}/cms`, authCMSRouter);
 
 // Mengunakan API orders
-app.use(v1, ordersRouter);
+app.use(`${v1}/cms`, ordersRouter);
+
+// Mengunakan API participants
+app.use(v1, participantsRouter);
 
 
 // Menggunakan handler error
