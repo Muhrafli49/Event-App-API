59cbc3320040e9e3e81f0a3cf2f647cb03e9fee6
Rafli <mraflie49@gmail.com>
2024-09-04 20:34:10 +0700

    Add orderMail function to send order summaries via email

--- app/services/mail/index.js
+++ app/services/mail/index.js
@@ -32,5 +32,25 @@ const otpMail = async (email, data) => {
     }
 };
 
+const orderMail = async (email, data) => {
+    try {
+        let template = fs.readFileSync('app/views/email/order.html', 'utf8');
+
+        let message = {
+            from: gmail,
+            to: email,
+            subject: 'Your Order Summaries',
+            html: Mustache.render(template, data),
+        };
+
+    return await transporter.sendMail(message);
+
+    } catch (ex) {
+        console.log(ex);
+    }
+};
+
 module.exports = { 
-    otpMail };
+    otpMail,
+    orderMail
+};
