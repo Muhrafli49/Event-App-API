94faaadf909c9d5d700d6195f61cb962addc1d47
Rafli <mraflie49@gmail.com>
2024-10-19 14:33:19 +0700

    update

--- app/api/images/router.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express();
-const { create } = require('./controllers');
-
-const upload = require('../../middleware/multer');
-
-router.post('/images',upload.single('avatar'), create);
-
-module.exports = router;
