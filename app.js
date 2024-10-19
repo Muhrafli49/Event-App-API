774c8f4aba8f882779ed32be3c883c6434761d5b
Rafli <mraflie49@gmail.com>
2024-10-19 14:32:54 +0700

    import and use new router

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 
 const app = express();
@@ -42,7 +43,7 @@ const paymentsRouter = require('./app/api/v1/payments/router');
 const notFoundMiddleware = require('./app/middleware/not-found');
 const handlerErrorMiddleware = require('./app/middleware/handler-error');
 
-
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
