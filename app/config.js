fd6c4c713a2d4f75547d3c3f68ecf10c238e1e24
Rafli <mraflie49@gmail.com>
2024-11-04 12:45:45 +0700

    update code

--- app/config.js
+++ app/config.js
@@ -1,10 +1,13 @@
 const dotenv = require('dotenv');
+const { createRefreshJWT } = require('./utils');
 dotenv.config();
 
 module.exports = {
     urlDb: process.env.URL_MONGODB_DEV,
     jwtExpiration: '24h',
-    jwtSecret: process.env.JWT_SECRET,
+    jwtSecret: process.env.JWT_SECRET_KEY,
+    jwtRefreshTokenExpiration: process.env.JWT_REFRESH_TOKEN_EXPIRATION,
+    jwtRefreshTokenSecret: process.env.JWT_REFRESH_TOKEN_SECRET_KEY,
     gmail: process.env.GMAIL,
     password: process.env.PASSWORD,
 };
